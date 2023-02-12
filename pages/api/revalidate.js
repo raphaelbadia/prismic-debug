961afc3519737cdfc11a873047c11b276320c4d4
Raphaël Badia <raphael@badia.cc>
2023-02-12 19:13:11 +0100

    debug

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -33,6 +33,7 @@ export default async function handler(req, res) {
 
       return res.json({ revalidated: true })
     } catch (err) {
+        console.log(err);
       // If there was an error, Next.js will continue to show
       // the last successfully generated page
       return res.status(500).send('Error revalidating')
